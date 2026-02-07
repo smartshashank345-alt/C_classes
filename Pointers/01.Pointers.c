ccb6e885cf54a074cde699c716a90cc48f026fd4
Arvinder Pal <arvinderpal10@gmail.com>
2026-02-07 23:05:12 +0530

    Pointers in C

---
+++ Pointers/01.Pointers.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main()
+{
+    // int x = 30;
+    // int *y = &x;
+    // int **z = &y;
+    // printf("x is : %d \n", x);
+    // printf("&x is :%p \n", &x);
+    // printf("y is :%p\n", y);
+    // printf("*y is : %d \n", *y);
+    // printf("&y is :%p \n", &y);
+
+    // int arr[5] = {10, 20, 30, 40, 50};
+    // int *ptr = arr;
+    // printf("arr address :%p \n", ptr);
+    // printf("arr[0] address :%p \n", &arr[0]);
+    // printf("arr address :%p \n", (ptr + 1));
+    // printf("arr[0] address :%p \n", &arr[1]);
+
+    // arr = (int *)malloc(n * sizeof(int))
+    return 0;
+}
